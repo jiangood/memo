c5eb403fcdddd433a22b7d8d3378c3f7f871eb47
jiangood <jiangood@users.noreply.github.com>
2026-05-28 00:07:52 +0000

    chore: bump version to 2.5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "fumi.day.literalmemo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "2.5.4"
+        versionCode = 26
+        versionName = "2.5.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
